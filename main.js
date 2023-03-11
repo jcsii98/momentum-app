5b10a497b1808d399e95d5ab61486cc6b8344850
jcsii98 <102233220+jcsii98@users.noreply.github.com>
2023-03-11 12:26:11 +0800

    update files via upload
    added some css styling, js for functionality and api

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// get references to input fields and forms
+// get references to input fields and 
 const nameFormPage = document.getElementById("intro-page-1");
 const emailFormPage = document.getElementById("intro-page-2");
 const nameForm = document.getElementById("intro-form-1");
@@ -18,6 +18,12 @@ nameForm.addEventListener("submit", (event) => {
     // show email form
     emailFormPage.classList.remove("hidden");
 
+    // concatenate name into email-greeting 
+    
+    const name = document.getElementById("name").value;
+    let emailString = `What's your email, ${name}?`;
+    document.getElementById("email-greeting").innerHTML = emailString;
+    
     // set focus to email input field
     emailInput.focus();
 
@@ -31,13 +37,61 @@ emailForm.addEventListener("submit", (event) => {
 
     // hide email page
     emailFormPage.classList.add("hidden");
+
     // get the users name and email
 
-    const name = nameInput.value;
-    const email = emailInput.value;
     
     // show welcome page
 
     const welcomePage = document.getElementById("welcome-page");
     welcomePage.classList.remove("hidden");
+}) 
+
+// show main-page
+
+const mainPage = document.getElementById("main-page");
+const procButton = document.getElementById("proceed-button");
+const welcomePage = document.getElementById("welcome-page");
+const footer = document.querySelector("#main-footer");
+
+procButton.addEventListener('click', function() {
+   
+    welcomePage.classList.add("hidden");
+    mainPage.classList.remove("hidden");
+    footer.classList.remove("hidden");
+    // get name for main-page
+    const name = document.getElementById("name").value;
+    
+    // get time for greeting
+
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();      
+    
+   if (currentHour < 12) {
+    document.getElementById("greeting").textContent = `Good morning, ${name}.`;
+  } else if (currentHour < 18) {
+    document.getElementById("greeting").textContent = `Good afternoon, ${name}.`;
+  } else {
+    document.getElementById("greeting").textContent = `Good evening, ${name}.`;
+  }
 })
+
+// time API
+
+  // update time 
+  function updateTime() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://worldtimeapi.org/api/timezone/Etc/UTC', true);
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var response = JSON.parse(request.responseText);
+      var datetime = response.utc_datetime;
+      var currentTime = new Date(datetime);
+      document.getElementById("currentTime").textContent = currentTime.toLocaleTimeString();
+    }
+  };
+  request.send();
+}
+  setInterval(updateTime, 1000);
+
+
